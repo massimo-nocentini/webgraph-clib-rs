21672753d61a4e1196dfd8c5086e6da227ec7af3
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-11-17 11:50:10 +0100

    Initialize project with Cargo configuration, build script, and C bindings

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+use libc::size_t;
+use webgraph::prelude::*;
+
+#[inline]
+#[allow(clippy::not_unsafe_ptr_arg_deref)]
+fn from_char_buff<'a>(ptr: *const u8, len: size_t) -> Option<&'a str> {
+    // SAFETY: We need to make sure that pointer is not null.
+    if ptr.is_null() {
+        None
+    } else {
+        unsafe {
+            let buff = std::slice::from_raw_parts(ptr, len as usize);
+            // SAFETY: Using the standard from_utf8 function, we ensure that the sequence is a valid string or NONE.
+            // In case of invalid sequence of strings the function returns None.
+            // Considering that the pointer is also not null there is no scenario under which the return value of this function won't be a proper string.
+            // There is no chance for an undefined behaviour.
+            // Therefor the return value of this function is both safe and sound.
+            // As a consequence not_unsafe_ptr_arg_deref warning is not relevant and has to be supressed.
+            std::str::from_utf8(buff).ok()
+        }
+    }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn a(str: *const u8, len: size_t, nodes: *mut size_t, arcs: *mut u64) {
+    let graph_filename = from_char_buff(str, len).unwrap();
+    let graph = BvGraph::with_basename(graph_filename).load().unwrap();
+
+    let n = graph.num_nodes();
+    let m = graph.num_arcs();
+
+    unsafe {
+        if !nodes.is_null() {
+            *nodes = n;
+        }
+        if !arcs.is_null() {
+            *arcs = m;
+        }
+    }
+}
